1c59f072d699a76f90bb10a1cddc6c2ce96fd498
Ktototam228 <evteevartem4@gmail.com>
2026-02-19 22:51:59 +0300

    Asylumgram v2 global chat

--- server.js
+++ server.js
@@ -8,57 +8,29 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let users = {}; // username -> socket.id
-
 io.on("connection", (socket) => {
 
-  socket.on("join", (username) => {
-    users[username] = socket.id;
-    io.emit("users", Object.keys(users));
+  socket.on("join", (name) => {
+    socket.username = name;
+
+    io.emit("system_message", `${name} вошёл в чат`);
   });
 
-  socket.on("private_message", (data) => {
-    const targetId = users[data.to];
-    if (targetId) {
-      io.to(targetId).emit("private_message", {
-        from: data.from,
-        message: data.message
-      });
-    }
-  });
-
-  socket.on("call-user", (data) => {
-    const targetId = users[data.to];
-    if (targetId) {
-      io.to(targetId).emit("incoming-call", data);
-    }
-  });
-
-  socket.on("answer-call", (data) => {
-    const targetId = users[data.to];
-    if (targetId) {
-      io.to(targetId).emit("call-answered", data);
-    }
-  });
-
-  socket.on("ice-candidate", (data) => {
-    const targetId = users[data.to];
-    if (targetId) {
-      io.to(targetId).emit("ice-candidate", data.candidate);
-    }
+  socket.on("chat_message", (msg) => {
+    io.emit("chat_message", {
+      user: socket.username,
+      message: msg
+    });
   });
 
   socket.on("disconnect", () => {
-    for (let name in users) {
-      if (users[name] === socket.id) {
-        delete users[name];
-      }
+    if (socket.username) {
+      io.emit("system_message", `${socket.username} вышел из чата`);
     }
-    io.emit("users", Object.keys(users));
   });
 
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
+server.listen(3000, () => {
+  console.log("Asylumgram running on port 3000");
 });
