2d33d4f587b23182d1b2fd64d47f5ddf35ca774c
Ktototam228 <evteevartem4@gmail.com>
2026-02-20 00:30:48 +0300

    Rebuilt index clean

--- server.js
+++ server.js
@@ -4,61 +4,78 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { maxHttpBufferSize: 1e8 });
+const io = new Server(server);
 
 app.use(express.static("public"));
 
-let users = {}; // username -> socket.id
+const ADMIN = "Admin"; // ← поменяй на свой ник
+
+let users = {};
 let privateMessages = {};
 
 function getRoom(a,b){
   return [a,b].sort().join("_");
 }
 
-function generateUniqueName(name){
-  let newName = name;
-  while(users[newName]){
-    newName = name + "#" + Math.floor(Math.random()*9999);
-  }
-  return newName;
-}
-
 io.on("connection",(socket)=>{
 
-  socket.on("join", name=>{
-    const uniqueName = generateUniqueName(name);
-    socket.username = uniqueName;
-    users[uniqueName] = socket.id;
-
-    socket.emit("your_name", uniqueName);
-    io.emit("users", Object.keys(users));
+  socket.on("register",(username,callback)=>{
+    if(users[username]){
+      callback({success:false,message:"Ник уже занят"});
+    } else {
+      socket.username=username;
+      users[username]=socket.id;
+      callback({success:true});
+      io.emit("users",Object.keys(users));
+    }
   });
 
-  socket.on("send_global", msg=>{
-    io.emit("new_global", msg);
+  socket.on("send_global",msg=>{
+    // --- команда кика ---
+    if(msg.content.startsWith("/kick ")){
+      if(socket.username !== ADMIN){
+        return;
+      }
+
+      const target = msg.content.split(" ")[1];
+
+      if(users[target]){
+        const targetSocket = io.sockets.sockets.get(users[target]);
+
+        if(targetSocket){
+          targetSocket.emit("kicked","Вы были кикнуты администратором");
+          targetSocket.disconnect();
+        }
+      }
+      return;
+    }
+
+    io.emit("new_global",msg);
   });
 
-  socket.on("send_private", ({to,message})=>{
-    const room = getRoom(socket.username,to);
+  socket.on("send_private",({to,message})=>{
+    const room=getRoom(socket.username,to);
     if(!privateMessages[room]) privateMessages[room]=[];
     privateMessages[room].push(message);
 
     if(users[to])
-      io.to(users[to]).emit("new_private", message);
+      io.to(users[to]).emit("new_private",message);
 
-    socket.emit("new_private", message);
+    socket.emit("new_private",message);
   });
 
-  socket.on("load_private", other=>{
-    const room = getRoom(socket.username,other);
-    socket.emit("private_history", privateMessages[room]||[]);
+  socket.on("load_private",other=>{
+    const room=getRoom(socket.username,other);
+    socket.emit("private_history",privateMessages[room]||[]);
   });
 
-  socket.on("disconnect", ()=>{
-    delete users[socket.username];
-    io.emit("users", Object.keys(users));
+  socket.on("disconnect",()=>{
+    if(socket.username){
+      delete users[socket.username];
+      io.emit("users",Object.keys(users));
+    }
   });
 
 });
 
-server.listen(3000,()=>console.log("Asylumgram v7 running"));
+server.listen(3000,()=>console.log("Asylumgram with kick system running"));
