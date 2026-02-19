2f42b8cc6a3bfb5f20c190ba0a28c5f12c17b39a
Ktototam228 <evteevartem4@gmail.com>
2026-02-19 23:05:02 +0300

    Asylumgram v3 full messenger

--- server.js
+++ server.js
@@ -8,29 +8,71 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+let users = {}; // name -> socket.id
+let messages = {
+  global: [],
+  private: {} // "user1_user2": []
+};
+
+function getPrivateRoom(a, b) {
+  return [a, b].sort().join("_");
+}
+
 io.on("connection", (socket) => {
 
   socket.on("join", (name) => {
     socket.username = name;
+    users[name] = socket.id;
 
-    io.emit("system_message", `${name} вошёл в чат`);
+    io.emit("users", Object.keys(users));
+    socket.emit("load_global", messages.global);
   });
 
-  socket.on("chat_message", (msg) => {
-    io.emit("chat_message", {
+  socket.on("send_global", (msg) => {
+    const data = {
       user: socket.username,
       message: msg
-    });
+    };
+
+    messages.global.push(data);
+    io.emit("new_global", data);
+  });
+
+  socket.on("send_private", ({ to, message }) => {
+    const room = getPrivateRoom(socket.username, to);
+
+    if (!messages.private[room]) {
+      messages.private[room] = [];
+    }
+
+    const data = {
+      from: socket.username,
+      to,
+      message
+    };
+
+    messages.private[room].push(data);
+
+    const targetId = users[to];
+    if (targetId) {
+      io.to(targetId).emit("new_private", data);
+    }
+
+    socket.emit("new_private", data);
+  });
+
+  socket.on("load_private", (otherUser) => {
+    const room = getPrivateRoom(socket.username, otherUser);
+    socket.emit("private_history", messages.private[room] || []);
   });
 
   socket.on("disconnect", () => {
-    if (socket.username) {
-      io.emit("system_message", `${socket.username} вышел из чата`);
-    }
+    delete users[socket.username];
+    io.emit("users", Object.keys(users));
   });
 
 });
 
 server.listen(3000, () => {
-  console.log("Asylumgram running on port 3000");
+  console.log("Asylumgram v3 running on port 3000");
 });
