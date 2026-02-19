8312b6106cf652c1b822d95f916e98d2e01b02d2
Ktototam228 <evteevartem4@gmail.com>
2026-02-20 00:13:04 +0300

    Asylumgram v7 PRO

--- server.js
+++ server.js
@@ -4,24 +4,33 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  maxHttpBufferSize: 1e8
-});
+const io = new Server(server, { maxHttpBufferSize: 1e8 });
 
 app.use(express.static("public"));
 
 let users = {}; // username -> socket.id
-let privateMessages = {}; // room -> messages[]
+let privateMessages = {};
 
 function getRoom(a,b){
   return [a,b].sort().join("_");
 }
 
-io.on("connection", (socket)=>{
+function generateUniqueName(name){
+  let newName = name;
+  while(users[newName]){
+    newName = name + "#" + Math.floor(Math.random()*9999);
+  }
+  return newName;
+}
 
-  socket.on("join", username=>{
-    socket.username = username;
-    users[username] = socket.id;
+io.on("connection",(socket)=>{
+
+  socket.on("join", name=>{
+    const uniqueName = generateUniqueName(name);
+    socket.username = uniqueName;
+    users[uniqueName] = socket.id;
+
+    socket.emit("your_name", uniqueName);
     io.emit("users", Object.keys(users));
   });
 
@@ -29,23 +38,20 @@ io.on("connection", (socket)=>{
     io.emit("new_global", msg);
   });
 
-  socket.on("send_private", ({to, message})=>{
-    const room = getRoom(socket.username, to);
-
-    if(!privateMessages[room]) privateMessages[room] = [];
-
+  socket.on("send_private", ({to,message})=>{
+    const room = getRoom(socket.username,to);
+    if(!privateMessages[room]) privateMessages[room]=[];
     privateMessages[room].push(message);
 
-    if(users[to]){
+    if(users[to])
       io.to(users[to]).emit("new_private", message);
-    }
 
     socket.emit("new_private", message);
   });
 
   socket.on("load_private", other=>{
-    const room = getRoom(socket.username, other);
-    socket.emit("private_history", privateMessages[room] || []);
+    const room = getRoom(socket.username,other);
+    socket.emit("private_history", privateMessages[room]||[]);
   });
 
   socket.on("disconnect", ()=>{
@@ -55,4 +61,4 @@ io.on("connection", (socket)=>{
 
 });
 
-server.listen(3000, ()=>console.log("Asylumgram v6 running"));
+server.listen(3000,()=>console.log("Asylumgram v7 running"));
