cd8b9c8471017a0008a55ffdbaa65fa25683e0ac
Ktototam228 <evteevartem4@gmail.com>
2026-02-20 15:35:03 +0300

    new clean build

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const fs = require("fs");
-const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(cors());
 app.use(express.static("public"));
 app.use(express.json({ limit: "50mb" }));
 
@@ -16,22 +14,11 @@ let users = {};
 let online = {};
 
 if (fs.existsSync("users.json")) {
-  try {
-    users = JSON.parse(fs.readFileSync("users.json"));
-  } catch {
-    users = {};
-  }
+  users = JSON.parse(fs.readFileSync("users.json"));
 }
 
 function saveUsers() {
   fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
-}
-
-function getOnlineUsers() {
-  return Object.keys(online).map(u => ({
-    username: u,
-    avatar: users[u]?.avatar || null
-  }));
 }
 
 io.on("connection", socket => {
@@ -67,56 +54,50 @@ io.on("connection", socket => {
       avatar: users[data.username].avatar
     });
 
-    io.emit("online", getOnlineUsers());
+    io.emit("online", Object.keys(online));
   });
 
   socket.on("setAvatar", img => {
     if (!socket.username) return;
     users[socket.username].avatar = img;
     saveUsers();
-    io.emit("online", getOnlineUsers());
   });
 
   socket.on("send_message", data => {
     if (!socket.username) return;
 
     if (data.to === "global") {
-      io.emit("new_message", data);
+      io.emit("new_message", {
+        from: socket.username,
+        text: data.text,
+        image: data.image || null
+      });
     } else {
       const id = online[data.to];
-      if (id) io.to(id).emit("new_message", data);
-      io.to(socket.id).emit("new_message", data);
+      if (id) {
+        io.to(id).emit("new_message", {
+          from: socket.username,
+          text: data.text,
+          image: data.image || null
+        });
+      }
+
+      io.to(socket.id).emit("new_message", {
+        from: socket.username,
+        text: data.text,
+        image: data.image || null
+      });
     }
-  });
-
-  // --- ЗВОНКИ ---
-
-  socket.on("callUser", ({ to, offer }) => {
-    const id = online[to];
-    if (id) io.to(id).emit("incomingCall", { from: socket.username, offer });
-  });
-
-  socket.on("answerCall", ({ to, answer }) => {
-    const id = online[to];
-    if (id) io.to(id).emit("callAnswered", { answer });
-  });
-
-  socket.on("iceCandidate", ({ to, candidate }) => {
-    const id = online[to];
-    if (id) io.to(id).emit("iceCandidate", { candidate });
   });
 
   socket.on("disconnect", () => {
     if (socket.username) {
       delete online[socket.username];
-      io.emit("online", getOnlineUsers());
+      io.emit("online", Object.keys(online));
     }
   });
 
 });
 
 const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log("Server started on port " + PORT);
-});
+server.listen(PORT, () => console.log("Server started"));
