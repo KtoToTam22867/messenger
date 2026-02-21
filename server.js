457baa14a4bc4bc0db6e2bdd7b8519c03c87cca6
Ktototam228 <evteevartem4@gmail.com>
2026-02-21 13:15:52 +0300

    asylumgram

--- server.js
+++ server.js
@@ -1,77 +1,247 @@
 const express = require("express");
+
 const http = require("http");
+
 const { Server } = require("socket.io");
+
 const fs = require("fs");
 
+
+
 const app = express();
+
 const server = http.createServer(app);
+
 const io = new Server(server);
+
+
 
 app.use(express.static("public"));
 
-const DB_FILE = "users.json";
+app.use(express.json({ limit: "50mb" }));
+
+
+
 let users = {};
+
 let online = {};
 
-if (fs.existsSync(DB_FILE)) {
-    try { users = JSON.parse(fs.readFileSync(DB_FILE)); } catch (e) { users = {}; }
-} else {
-    fs.writeFileSync(DB_FILE, JSON.stringify({}));
+
+
+if (fs.existsSync("users.json")) {
+
+users = JSON.parse(fs.readFileSync("users.json"));
+
 }
 
-io.on("connection", (socket) => {
-    socket.on("register", (data, cb) => {
-        if (!data.username || users[data.username]) return cb({ success: false, msg: "Логин занят" });
-        users[data.username] = { password: data.password };
-        fs.writeFileSync(DB_FILE, JSON.stringify(users, null, 2));
-        cb({ success: true });
-    });
 
-    socket.on("login", (data, cb) => {
-        if (users[data.username] && users[data.username].password === data.password) {
-            socket.username = data.username;
-            online[data.username] = socket.id;
-            cb({ success: true });
-            io.emit("online", Object.keys(online));
-        } else cb({ success: false, msg: "Ошибка доступа" });
-    });
 
-    socket.on("send_message", (d) => {
-        const payload = { 
-            from: socket.username, 
-            text: d.text, 
-            to: d.to, 
-            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) 
-        };
-        if (d.to === "global") {
-            io.emit("new_message", payload);
-        } else {
-            const sid = online[d.to];
-            if (sid) io.to(sid).emit("new_message", payload);
-            socket.emit("new_message", payload);
-        }
-    });
+function saveUsers() {
 
-    // Сигналинг WebRTC
-    socket.on("call-user", d => {
-        if(online[d.to]) io.to(online[d.to]).emit("incoming-call", { from: socket.username, offer: d.offer });
-    });
-    socket.on("answer-call", d => {
-        if(online[d.to]) io.to(online[d.to]).emit("call-answered", { answer: d.answer });
-    });
-    socket.on("ice-candidate", d => {
-        if(online[d.to]) io.to(online[d.to]).emit("ice-candidate", { candidate: d.candidate });
-    });
-    socket.on("reject-call", d => {
-        if(online[d.to]) io.to(online[d.to]).emit("call-rejected");
-    });
+fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
 
-    socket.on("disconnect", () => {
-        if (socket.username) {
-            delete online[socket.username];
-            io.emit("online", Object.keys(online));
-        }
-    });
+}
+
+
+
+io.on("connection", socket => {
+
+
+
+socket.on("register", (data, cb) => {
+
+if (!data.username || !data.password)
+
+return cb({ success: false });
+
+
+
+if (users[data.username])
+
+return cb({ success: false });
+
+
+
+users[data.username] = {
+
+password: data.password,
+
+avatar: null
+
+};
+
+
+
+saveUsers();
+
+cb({ success: true });
+
 });
 
-server.listen(3000, '0.0.0.0', () => console.log("🚀 Сервер запущен на порту 3000"));
+
+
+socket.on("login", (data, cb) => {
+
+if (!users[data.username])
+
+return cb({ success: false });
+
+
+
+if (users[data.username].password !== data.password)
+
+return cb({ success: false });
+
+
+
+socket.username = data.username;
+
+online[data.username] = socket.id;
+
+
+
+cb({
+
+success: true,
+
+avatar: users[data.username].avatar
+
+});
+
+
+
+io.emit("online", Object.keys(online));
+
+});
+
+
+
+socket.on("setAvatar", img => {
+
+if (!socket.username) return;
+
+users[socket.username].avatar = img;
+
+saveUsers();
+
+});
+
+
+
+socket.on("send_message", data => {
+
+if (!socket.username) return;
+
+
+
+const payload = {
+
+from: socket.username,
+
+text: data.text || "",
+
+image: data.image || null
+
+};
+
+
+
+if (data.to === "global") {
+
+io.emit("new_message", payload);
+
+} else {
+
+const id = online[data.to];
+
+if (id) io.to(id).emit("new_message", payload);
+
+io.to(socket.id).emit("new_message", payload);
+
+}
+
+});
+
+
+
+// ===== ЗВОНКИ =====
+
+
+
+socket.on("call-user", ({ to, offer }) => {
+
+const id = online[to];
+
+if (id) {
+
+io.to(id).emit("incoming-call", {
+
+from: socket.username,
+
+offer
+
+});
+
+}
+
+});
+
+
+
+socket.on("answer-call", ({ to, answer }) => {
+
+const id = online[to];
+
+if (id) {
+
+io.to(id).emit("call-answered", {
+
+answer
+
+});
+
+}
+
+});
+
+
+
+socket.on("ice-candidate", ({ to, candidate }) => {
+
+const id = online[to];
+
+if (id) {
+
+io.to(id).emit("ice-candidate", {
+
+candidate
+
+});
+
+}
+
+});
+
+
+
+socket.on("disconnect", () => {
+
+if (socket.username) {
+
+delete online[socket.username];
+
+io.emit("online", Object.keys(online));
+
+}
+
+});
+
+
+
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log("Server started"));
